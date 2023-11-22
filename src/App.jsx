72a466b0a2d0047ccc29c10d18135abd7218f428
raulgp <raul.garrigos@hotmail.com>
2023-11-22 16:20:54 +0100

    Iteration 1 done - Creo. Ya veremos si bien

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
 
 function App() {
+  const [showFood, setShowFood] = useState(foodsJson);
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
