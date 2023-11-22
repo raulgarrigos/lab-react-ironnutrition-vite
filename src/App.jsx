4eee21aae5957ce147407a275f3033c2080aa43f
raulgp <raul.garrigos@hotmail.com>
2023-11-22 16:25:07 +0100

    Iteration 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
 
 function App() {
   const [showFood, setShowFood] = useState(foodsJson);
@@ -8,6 +9,15 @@ function App() {
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+
+      <FoodBox
+        food={{
+          name: "Orange",
+          calories: 85,
+          image: "https://i.imgur.com/abKGOcv.jpg",
+          servings: 1,
+        }}
+      />
     </div>
   );
 }
